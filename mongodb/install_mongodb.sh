c896c31e543609a98f1cf0c7d8d2752691c9e474
Sanket Chaudhari <chaudharisanket2000@gmail.com>
2020-03-06 16:46:02 +0530

    Update install_mongodb.sh

--- mongodb/install_mongodb.sh
+++ mongodb/install_mongodb.sh
@@ -1,3 +1,5 @@
 cd /tmp
 wget https://downloads.mongodb.com/compass/mongodb-compass-1.14.5.x86_64.rpm
-sudo dnf install ./mongodb-compass-1.14.5.x86_64.rpm
+dnf install ./mongodb-compass-1.14.5.x86_64.rpm
+curl -S https://raw.githubusercontent.com/sanket143/IScripts/master/mongodb/install_mongodb-shell.repo > /etc/yum.repos.d/mongodb.repo
+dnf install mongodb-org-shell
